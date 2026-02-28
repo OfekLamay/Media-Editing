d95ad3b398e074d0c8c5de2271c62fa5f03c5887
Ofek Lamay <ofeklamay345@gmail.com>
2026-02-28 03:39:06 +0200

    Started creating the server

---
+++ video-editor-backend/src/index.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+import type { Request, Response } from 'express';
+
+
+const app = express();
+const PORT = 3003;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('This is the video editor backend!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
